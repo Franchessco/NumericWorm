8937af7691cb1949341254b3546513e29850074f
Franchessco <fzabier@gmail.com>
2023-11-28 00:01:07 +0100

    1) added Exeption: non setted models for dataModel and errorModel as fucntion poitners 2) added aliases in SimplexPoint for shorter and more redability 3) redefined data from const vector& -> shared pointer 4) added test for SimplexFigure for throwing exeption 5) initialized test for Contraction,Reflection,Expantion, Shrinking 6) added TODO

--- TestNumericStorm/TestFitting/TestingSimplex/TestingSimplexFigure.cpp
+++ TestNumericStorm/TestFitting/TestingSimplex/TestingSimplexFigure.cpp
@@ -18,5 +18,30 @@ namespace TestingSimplexFigure
 		EXPECT_EQ(simplexFigure[0], minBounds);
 		EXPECT_EQ(simplexFigure[3], maxBounds);
 	}
+
+
+	using vectorPointer = std::shared_ptr<std::vector<double>>;
+
+	using DataModel = std::vector<double>(*)(const vectorPointer args, Parameters<2>param);
+	using ErrorModel = double(*)(const vectorPointer mother, const std::vector<double>& child);
+	struct TestignSettingAndExecutingModel: public testing::Test 
+	{
+		Bounds<2> minBounds{1,1};
+		Bounds<2> maxBounds{2,2};
+
+		SimplexFigure<3> simplexFigure{ minBounds,maxBounds };
+
+	};
+
+TEST_F(TestignSettingAndExecutingModel, TestThrowError) {
+
+	try 
+	{simplexFigure.sort();}
+	catch (const std::exception& e) {
+		std::string messege = static_cast<std::string>(e.what());
+		EXPECT_STREQ("Data and Error no setted model", e.what());
+		//TODO add test for all possible error messages 
+	}
+}
 }
 
